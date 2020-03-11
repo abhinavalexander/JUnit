a778fd9d4e55f69d5a6dbf3ffd9e850230c8b662
Abhinav <abhinavalexandereme@gmail.com>
2020-03-11 15:51:19 +0530

    junit

---
+++ JavaNewFeatures/src/JunitDemo/Mymath.java
@@ -0,0 +1,27 @@
+package JunitDemo;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Mymath {
+public class MymathTest{
+	Mymath myMath=new Mymath();
+
+	@Test
+	public void sum_with3numbers(){
+		System.out.println("Test1");
+		assertEquals(6,Mymath.sum(new int[]{1,2,3}));
+		
+	}
+	@Test
+	public void sum_with1number(){
+		System.out.println("Test2");
+		assertEquals(3, Mymath.sum(new int[]{3}));
+	}
+}
+
+public static Object sum(int[] is) {
+	// TODO Auto-generated method stub
+	return null;
+}
+}
